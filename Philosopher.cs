6f53b21bef78d5a41ad888449ab82e43317a8ed8
1Igor6 <igorbiatlon16@gmail.com>
2026-06-22 23:49:41 +0300

    UPD

---
+++ Philosopher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace DiningPhilosophers
+{
+    class Philosopher
+    {
+        private int id;
+        private int leftFork, rightFork;
+        private Table table;
+
+        public Philosopher(int id, Table table)
+        {
+            this.id = id;
+            this.table = table;
+            this.rightFork = id;
+            this.leftFork = (id + 1) % 5;
+            new Thread(Run).Start();
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Philosopher {id} is thinking {i + 1} times");
+
+                while (true)
+                {
+                    table.CallWaiter();
+
+                    if (table.TryGetForks(rightFork, leftFork))
+                    {
+                        Console.WriteLine($"Philosopher {id} is eating {i + 1} times");
+                        table.PutForks(rightFork, leftFork);
+                        table.DismissWaiter();
+                        break;
+                    }
+
+                    table.DismissWaiter();
+                    Thread.Sleep(10);
+                }
+            }
+        }
+    }
+}
---
+++ Table.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace DiningPhilosophers
+{
+    class Table
+    {
+        private SemaphoreSlim[] forks = new SemaphoreSlim[5];
+        
+        private SemaphoreSlim tokens = new SemaphoreSlim(2, 2);
+
+        public Table()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                forks[i] = new SemaphoreSlim(1, 1);
+            }
+        }
+
+        public void CallWaiter() { tokens.Wait(); }
+        public void DismissWaiter() { tokens.Release(); }
+
+        
+        public bool TryGetForks(int right, int left)
+        {
+            if (forks[right].Wait(0)) 
+            {
+                if (forks[left].Wait(0)) 
+                {
+                    return true; 
+                }
+                forks[right].Release(); 
+            }
+            return false; 
+        }
+
+        public void PutForks(int right, int left)
+        {
+            forks[left].Release();
+            forks[right].Release();
+        }
+    }
+}
